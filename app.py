45384c8688cde63a467bb2cd9a4e493a861504cd
shakib1729 <39847281+shakib1729@users.noreply.github.com>
2020-07-01 16:05:59 +0530

    adding CNN model

--- app.py
+++ app.py
@@ -2,48 +2,53 @@ from flask import Flask, jsonify, request
 import os
 import numpy as np
 import librosa
-
+from PIL import Image
 from keras.models import load_model
 from werkzeug.utils import secure_filename
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
 
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-MODEL_PATH = 'models/UrbanSoundComplete1.h5'
+MODEL_PATH = 'models/UrbanSoundCompleteCNNAdam.h5'
 model = load_model(MODEL_PATH)
 
+def save_spectrogram(curr_audio_path, curr_audio_name):
+    X, sr = librosa.load(curr_audio_path)  # librosa.load() returns an np array and sampling rate(by default 22050)
+    plt.specgram(X, Fs=22050)
+    plt.gca().axes.get_yaxis().set_visible(False)
+    plt.gca().axes.get_xaxis().set_visible(False)
+    plt.plot
+    plt.savefig(curr_audio_name,  bbox_inches= 'tight' , pad_inches = 0, dpi = 25)
 
-def extract_mfcc(y, sr):
-    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
-    mfccs = np.mean(mfccs.T, axis=0)
-    return mfccs
+class_names = ['air_conditioner', 'car_horn', 'children_playing', 'dog_bark', 'drilling', 'engine_idling', 'gun_shot', 'jackhammer', 'siren', 'street_music']
 
 def model_predict(wav_path):
-    y, sr = librosa.load(wav_path)
-    x = extract_mfcc(y, sr)
-    x = np.expand_dims(x, axis = 0)
-    pred = model.predict(x)
-    class_idx = np.argmax(pred[0])
-    class_names = ['air_conditioner', 'car_horn', 'children_playing', 
-                   'dog_bark', 'drilling', 'engine_idling', 'gun_shot', 
-                   'jackhammer', 'siren', 'street_music']
-    predicted_class = class_names[class_idx]
-    return predicted_class
+	wav_name = os.path.splitext(wav_path)[0]
+	save_spectrogram(wav_path, wav_name)
+	png_path = wav_name + '.png'
+	png_img = image.load_img(png_path, target_size = (64, 64))
+	x = image.img_to_array(png_img)
+	x = np.expand_dims(x, axis = 0)
+	pred = model.predict(x)
+	class_idx = np.argmax(pred[0])
+	predicted_class = class_names[class_idx]
+	return predicted_class
 
 
 @app.route('/')
 def index():
     # Main page, nothing to render here
-    return "Hello From Server"
+    return "Hello From Server, Hii"
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
 	if request.method == 'POST':
 		# Get the file from the POST request
 		currFile = request.files['file']
-		print (request)
 		# Save the file to the specified path
 		file_name = secure_filename(currFile.filename)
 		file_path = os.path.join('./uploads', file_name)
